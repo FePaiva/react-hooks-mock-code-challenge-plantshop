9183d2489010367116aac46333a78be2bca2a614
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2021-12-21 14:32:32 -0800

    core deliverables done

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+
+  const [plants, setPlants] = useState ([])
+  const [filteredPlants, setFilteredPlants] = useState (plants)
+
+  function handleSearch(e){
+    const filteredPlants = plants.filter(plant => {
+      return plant.name.includes(e.target.value)
+    })
+    setFilteredPlants(filteredPlants)
+  }
+
+  function addNewPlant(newPlant){
+    const updatedPlants = [...plants, newPlant]
+    setPlants(updatedPlants)
+  }
+
+  useEffect (() => {
+    setFilteredPlants(plants)
+  }, [plants])
+
+  useEffect (() => {
+    fetch("http://localhost:6001/plants")
+    .then((r) => r.json())
+    .then (data => {
+      //doing this so the plants are loaded in the page.
+      setPlants(data)});
+  }, [],)
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant}/>
+      <Search handleSearch={handleSearch} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
